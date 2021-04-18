2014a7eeaa1e9b47214d16288614af8c6390f82d
souravjamwal77 <souravjamwal77@gmail.com>
2021-04-18 12:49:00 +0530

    Add Exercise 3.3, solution pending

---
+++ ch3/exercise_3_3.py
@@ -0,0 +1,21 @@
+""" This module contains the answer to exercise 3 of Chapter 3.
+"""
+
+def print_hyphens():
+    """ Prints 4 hyphen (-) symbol on the console.
+    """
+    print(" -" * 4, end=" ")
+
+
+def print_add_sign():
+    """Prints the Add(+) sign on the console.
+    """
+    
+
+
+
+def draw_grid():
+    """Draws a grid with - as lines and + as corner point.
+    """
+    print("+")
+    print("")
